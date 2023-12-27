da19b390cd98f74f6ed6c4540c5a446ce9ec1e3d
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2023-12-27 17:37:56 +0700

    ลบที่ไม่ได้ใช้และ สร้าง script ในการสร้างชื่อและควบคุมหน้าเมนู

--- Assets/Scripts/NotComplete/CreateNameUser.cs
+++ Assets/Scripts/NotComplete/CreateNameUser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,17 +6,47 @@ using UnityEngine;
 
 public class CreateNameUser : MonoBehaviour, IDataPersistances
 {
-    [SerializeField] TextMeshProUGUI _inputFields;
+    [SerializeField] TextMeshProUGUI _informationField;
+    [SerializeField] GameObject _objConfirmGroup;
+    [SerializeField] GameObject _objCreateGroup;
+    [SerializeField] TMP_InputField _inputNameField;
 
+    public string _name { get; private set; }
     public void LoadData(GameData gameData)
     {
-        _inputFields.text = gameData._playerName;
+        _name = gameData._playerName;
     }
 
     public void SaveData(ref GameData gameData)
     {
-        gameData._playerName = _inputFields.text;
+        gameData._playerName = _name;
     }
 
+    public void OnConfirmClick()
+    {
+        _objCreateGroup.SetActive(false);
+        _objConfirmGroup.SetActive(true);
+        _informationField.text = "คุณต้องการใช้ชื่อ " + _name + " ใช่หรือไม่";
+    }
+
+    public void OnCancelClick()
+    {
+        Debug.Log("Cac=ncel click");
+    }
+
+    public void OnYesClick()
+    {
+
+    }
+
+    public void OnNoClick()
+    {
+
+    }
+
+    private void Update()
+    {
+        _name = _inputNameField.text;
+    }
 
 }
