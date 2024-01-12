eb10d621d301c612b7e86c46dff5f89a70a8b980
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-01-12 17:07:55 +0700

    Update

--- Assets/Scripts/NotComplete/SchoolDay.cs
+++ Assets/Scripts/NotComplete/SchoolDay.cs
@@ -6,20 +6,31 @@ public class SchoolDay : MonoBehaviour
 {
     private bool isPlayTeacher = false;
     private bool dialogIsComplete = false;
+    [SerializeField] private GameObject cutsceneTimeline1;
+    [SerializeField] private GameObject cutsceneCamera1;
+    [SerializeField] private GameObject cutsceneTimeline2;
+    [SerializeField] private GameObject cutsceneCamera2;
+    [SerializeField] private GameObject police;
+    [SerializeField] private GameObject keyItem;
+
+    private bool classroomDialogIsComp;
+    private bool policeDialogIsComp;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        classroomDialogIsComp = false;
+        policeDialogIsComp = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isPlayTeacher && dialogIsComplete)
-        {
-            StartCoroutine(PlayDialogScene());
-        } 
+        //if (!isPlayTeacher && dialogIsComplete)
+        //{
+        //    StartCoroutine(PlayDialogScene());
+        //} 
+
     }
 
     IEnumerator PlayDialogScene()
